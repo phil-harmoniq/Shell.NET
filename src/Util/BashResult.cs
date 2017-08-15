3aa6ff076fce9e9aa69e491efbe3627c426e8803
Phil H <ph-hawkins@outlook.com>
2017-08-15 12:09:26 -0400

    Better XML docs

--- src/Util/BashResult.cs
+++ src/Util/BashResult.cs
@@ -3,23 +3,23 @@ using System;
 namespace Shell.NET.Util
 {
     /// <summary>
-    /// Simple container for the results of a Bash command. </summary>
+    /// Simple container for the results of a Bash command.</summary>
     public class BashResult
     {
         /// <summary>
-        /// The command output as a string. (if redirected) </summary>
+        /// The command's standard output as a string. (if redirected)</summary>
         public string Output { get; private set; }
 
         /// <summary>
-        /// The command error output as a string. (if redirected) </summary>
+        /// The command's error output as a string. (if redirected)</summary>
         public string ErrorMsg { get; private set; }
 
         /// <summary>
-        /// The command exit code as an integer. (if redirected) </summary>
+        /// The command's exit code as an integer.</summary>
         public int ExitCode { get; private set; }
 
         /// <summary>
-        /// An array of the command output split by newline characters. (if redirected) </summary>
+        /// An array of the command's output split by newline characters. (if redirected)</summary>
         public string[] Lines => Output?.Split('\n');
 
         internal BashResult(string output, string errorMsg, int exitCode)
